8cf9fe11d0ffb994a19c6112669c2add448d092e
pmgblz <paula.gablenz@gmail.com>
2019-11-28 07:18:00 -0800

    current R script

---
+++ code/methods.R
@@ -0,0 +1,543 @@
+
+# Packages 
+library(dplyr)       # Data manipulation (0.8.0.1)
+library(fBasics)     # Summary statistics (3042.89)
+library(corrplot)    # Correlations (0.84)
+library(psych)       # Correlation p-values (1.8.12)
+library(grf)         # Generalized random forests (0.10.2)
+library(rpart)       # Classification and regression trees, or CART (4.1-13)
+library(rpart.plot)  # Plotting trees (3.0.6)
+library(treeClust)   # Predicting leaf position for causal trees (1.1-7)
+library(car)         # linear hypothesis testing for causal tree (3.0-2)
+library(devtools)    # Install packages from github (2.0.1)
+library(readr)       # Reading csv files (1.3.1)
+library(tidyr)       # Database operations (0.8.3)
+library(tibble)      # Modern alternative to data frames (2.1.1)
+library(knitr)       # RMarkdown (1.21)
+library(kableExtra)  # Prettier RMarkdown (1.0.1)
+library(ggplot2)     # general plotting tool (3.1.0)
+library(haven)       # read stata files (2.0.0)
+library(aod)         # hypothesis testing (1.3.1)
+library(evtree)      # evolutionary learning of globally optimal trees (1.0-7)
+library(haven)
+library(data.table)
+library(caret)
+library(magick)
+library(xtable)
+library(stargazer)
+#library(HoRM)
+#library(KernSmooth)
+library(forecast)
+#library(Kendall)
+#library(trend)
+#library(aTSA)
+library(lmtest)
+library(nnfor)
+library(tsutils)
+
+library(smooth)
+library(Mcomp)
+
+
+# Run path
+if (dir.exists("/Users/paula/stats205_project/")) {
+  setwd("/Users/paula/stats205_project/")
+} else {
+  setwd("ENTER YOUR PATH HERE")
+}
+
+
+######## LOAD & CLEAN DATA ###########
+
+# Load data 
+day <- read_csv("data/day.csv")
+hour <- read_csv("data/hour.csv")
+
+# make sure it is ordered correctly 
+hour <- hour[order(hour$dteday, hour$hr),]
+day <- day[order(day$dteday),]
+
+
+# Load functions 
+source("code/kernel_functions.R")
+
+# Clean data 
+setDT(hour)
+hour[, season := as.factor(ifelse(season == 1, "Spring", 
+                                  ifelse(season == 2, "Summer", 
+                                         ifelse(season == 3, "Fall", 
+                                                ifelse(season == 4, "Winter", NA)))))]
+
+hour[, weathersit := as.factor(ifelse(weathersit == 1, "Clear", 
+                                ifelse(weathersit == 2, "Misty", 
+                                       ifelse(weathersit == 3, "Rain", 
+                                              ifelse(weathersit == 4, "Thunderstorm", NA)))))]
+
+hour <- hour[, -c("instant")]
+
+
+# dummify the data
+dmy <- dummyVars(" ~ .", data = hour)
+hour <- data.frame(predict(dmy, newdata = hour))
+
+
+# Drop rows containing missing values (doesn't actually do anything)
+hour <- na.omit(hour)
+
+# get total counts
+setDT(hour)
+setDT(day)
+
+# further cleaning 
+setDT(hour)
+hour[, yr := ifelse(hour$yr == 0, 2011, 2012)]
+
+hour_temp <- hour[, .(mean_count = mean(cnt)), by = c("temp")]
+hour_temp <- hour[, lapply(.SD, mean), by=temp]
+
+day[, month := mnth + yr*12]
+
+######## TEST & TRAIN DATA ###########
+set.seed(1)
+
+train_fraction <- 0.8
+
+# Day; test = last month (make sure it is ordered)
+day_train_forecast <- day[dteday <= "2012-11-30", ]
+day_test_forecast <- day[dteday > "2012-11-30", ]
+
+# day_train_forecast <- day[1:round(nrow(day)*train_fraction), ]
+# day_test_forecast <- day[(round(nrow(day)*train_fraction) + 1):nrow(day), ]
+
+######## DESCRIPTIVES ###########
+
+# Make a data.frame containing summary statistics of interest
+summ_stats <- fBasics::basicStats(hour[, -c("dteday")])
+summ_stats <- as.data.frame(t(summ_stats))
+
+# Rename some of the columns for convenience
+summ_stats <- summ_stats[c("Mean", "Stdev", "Minimum", "1. Quartile", "Median",  "3. Quartile", "Maximum")]
+colnames(summ_stats)[colnames(summ_stats) %in% c('1. Quartile', '3. Quartile')] <- c('Lower quartile', 'Upper quartile')
+
+row.names(summ_stats) <- c("Fall", "Spring", "Summer", "Winter", 
+                           "Year", "Month", "Hour",
+                           "Holiday", "Day of Week", "Workday", "Clear Weather", "Misty Weather", 
+                           "Rainy Weather", "Thunderstorm", "Temperature",
+                           "Felt Temperature", "Humidity", "Windspeed", "Casual Users", 
+                           "Registered Users", "All Users")
+
+
+
+# save
+stargazer(summ_stats,
+          type = "latex", 
+          summary=FALSE, rownames=TRUE,
+          digits = 2) -> sumstats 
+
+tabular_positions <- grep("tabular", sumstats)
+sumstats <- sumstats[tabular_positions[1]:tabular_positions[2]]
+write(sumstats,  file="output/tables/summary_stats.tex")
+
+hour_plot <- hour
+colnames(hour_plot) <- c("Date", "Fall", "Spring", "Summer", "Winter", 
+                  "Year", "Month", "Hour",
+                  "Holiday", "Day of Week", "Workday", "Clear Weather", "Misty Weather", 
+                  "Rainy Weather", "Thunderstorm", "Temperature",
+                  "Felt Temperature", "Humidity", "Windspeed", "Casual Users", 
+                  "Registered Users", "All Users")
+
+# correlation plot 
+pairwise_pvalues <- psych::corr.test(hour_plot, hour_plot)$p
+png(filename="output/plots/corrplot.png")
+pairwise_pvalues <- psych::corr.test(hour_plot, hour_plot)$p
+corrplot(cor(hour_plot),
+         type="upper",
+         tl.col="black",
+         order="hclust",
+         tl.cex=0.6,
+         addgrid.col = "black",
+         p.mat=pairwise_pvalues,
+         sig.level=0.05,
+         number.font=10,
+         insig="blank") 
+dev.off()
+
+# plot daily trends
+day %>% 
+  ggplot(aes(dteday, cnt)) + geom_point(size = 0.5) + 
+  xlab("Day") + ylab("Total Number of Users")
+
+ggsave("output/plots/users_by_day.png")
+
+# average number of users by hour 
+users_by_hour <- hour[, .(avg_users_by_hours = mean(cnt)), by = c("hr")]
+
+users_by_hour %>% 
+  ggplot(aes(hr, avg_users_by_hours)) + geom_line(size = 0.5) + 
+  xlab("Hour") + ylab("Average Number of Users")
+ggsave("output/plots/average_number_users_by_hour.png")
+
+
+
+######### SIMPLE MOVING AVERAGE ########### 
+
+numbins <- 5
+day_train_forecast$bins <- as.numeric(cut(day_train_forecast$dteday, numbins + 1))
+order_max <- 20
+mse_order <- matrix(nrow = order_max, ncol = 2)
+
+for(o in 1:order_max) {
+  
+  mse_cv <- c()
+  
+  for(i in 1:numbins) {
+    
+    train <- day_train_forecast[bins <= i, ]
+    test <- day_train_forecast[bins == i + 1, ]
+    
+    # run simple moving average on test set 
+    # hold out set is length of training set
+    mod <- sma(train$cnt, order = o, h = nrow(test))
+    
+    # calcualte mse 
+    mse_cv[i] <- mean((test$cnt -  forecast(mod, h = nrow(test))$mean[1:nrow(test)])^2)
+    
+    
+  }
+  
+  mse_order[o, 1] <- o
+  mse_order[o, 2] <- mean(mse_cv)
+}
+
+# which is the best simple moving average? 
+best_cv_order <- which.min(mse_order[, 2])
+
+# refit with best cv order (and predict on test set)
+best_sma <- sma(day_train_forecast$cnt, o = best_cv_order,
+                h = nrow(day_test_forecast), interval = "np")
+plot(forecast(best_sma, h = nrow(day_test_forecast) ))
+
+
+########## CUBIC SPLINES ################# 
+
+# this does cross validation: https://www.rdocumentation.org/packages/forecast/versions/8.9/topics/splinef
+# same as arima (0, 2, 2)
+# shown how cubic smoothing splines can be used to obtain local linear forecasts for a univariate time series
+cubic_smoothing_spline <- splinef(day_train_forecast$cnt,  h = nrow(day_test_forecast))
+fcast_cubic_spline <- forecast(cubic_smoothing_spline,  h = nrow(day_test_forecast))
+plot(fcast_cubic_spline)
+
+
+######## EXPONENTIAL SMOOTHING #############
+
+day_train_forecast$bins_ets <- as.numeric(cut(day_train_forecast$dteday, numbins + 1))
+
+cross_validate_ets <- function(er, tr, sea = "N", numbins = 5) {
+  
+  mse_cv <- c()
+  
+  for(i in 1:numbins) {
+    
+    train <- day_train_forecast[bins <= i, ]
+    test <- day_train_forecast[bins == i + 1, ]
+    
+    # run simple moving average on test set 
+    # hold out set is length of training set
+    mod <- ets(train$cnt, model = paste0(er, tr, sea))
+    
+    # calcualte mse 
+    mse_cv[i] <- mean((test$cnt -  forecast(mod, h = nrow(test))$mean[1:nrow(test)])^2)
+    
+    
+  }
+  
+  
+  return(mean(mse_cv))
+}
+
+
+error_type <- c("A", "M")
+trend_type <- c("A", "M", "N")
+#season_type <- c("A", "M", "N")
+
+mse_ets <- c()
+c <- 0
+for(er in error_type) {
+  for(tr in trend_type) {
+    for(sea in season_type) {
+      c <- c + 1
+      mse_ets[c] <- cross_validate_ets(er, tr, sea)
+      
+    }
+  }
+  
+}
+
+
+
+
+
+
+
+fit <- ets(day_train_forecast$cnt)
+plot(forecast(fit))
+
+# 
+# day_ts <- ts(day$cnt, start = c(2011, 1, 1), end = c(2012, 12, 31), frequency = 7)
+# 
+# # weekly, monthly and yearly seasonality
+# y <- msts(day_train_forecast$cnt, seasonal.periods=c(1, 7,30.5, 365.25), start = c(2011,01) )
+
+
+
+# Multi-step, re-estimation
+#x <- msts(day$cnt, seasonal.periods=c(7, 365.25))
+# SHIFT FOREWARD BY 7 OBSERVATIONS (ONE WEEK)
+x <- ts(day$cnt, start = c(2011, 1), deltat = 1/(365.5))
+k <- 7 # minimum data length for fitting a model (ONE WEEK)
+n <- length(day$cnt)
+f <- 365.5
+mae1 <- mae2 <- mae3 <- matrix(NA,f,f)
+st <- tsp(x)[1]+(k-1)/f
+for(i in 1:shift)
+{
+  xshort <- window(x, end=st + (i-1))
+  xnext <- window(a10, start=st + (i-1) + 1/12, end=st + i)
+  fit1 <- tslm(xshort ~ trend + season, lambda=0)
+  fcast1 <- forecast(fit1, h=12)
+  fit2 <- Arima(xshort, order=c(3,0,1), seasonal=list(order=c(0,1,1), period=12),
+                include.drift=TRUE, lambda=0, method="ML")
+  fcast2 <- forecast(fit2, h=12)
+  fit3 <- ets(xshort,model="MMM",damped=TRUE)
+  fcast3 <- forecast(fit3, h=12)
+  mae1[i,] <- abs(fcast1[['mean']]-xnext)
+  mae2[i,] <- abs(fcast2[['mean']]-xnext)
+  mae3[i,] <- abs(fcast3[['mean']]-xnext)
+}
+plot(1:12, colMeans(mae1), type="l", col=2, xlab="horizon", ylab="MAE",
+     ylim=c(0.35,1.5))
+lines(1:12, colMeans(mae2), type="l",col=3)
+lines(1:12, colMeans(mae3), type="l",col=4)
+legend("topleft",legend=c("LM","ARIMA","ETS"),col=2:4,lty=1)
+
+
+#### Starting with 1 month of training data
+#### We think it has weekly frequency, forecast for a week
+dt <- day
+i <- 30  
+forecast_length <- 48
+pred_ets <- c()
+#pred_arima <- c()
+while(i <= nrow(dt)){
+  
+  ts <- ts(dt[1:i, "cnt"], frequency=7)
+  
+  #pred_ets <- rbind(pred_ets, data.frame(forecast(ets(ts), 7)$mean[1:3]))
+  pred_arima <- rbind(pred_arima, data.frame(forecast(auto.arima(ts), forecast_length)$mean[1:forecast_length]))
+  
+  i = i + forecast_length
+  
+}
+names(pred_arima) <- "arima"
+names(pred_ets) <- "ets"
+
+pred_ets <- ts(pred_ets$ets, start=c(2005, 01), frequency = 12)
+pred_arima <- ts(pred_arima$arima, start=c(2005, 01), frequency =12)
+
+accuracy(pred_ets, ts_dt)
+accuracy(pred_arima, ts_dt)
+
+######## UNIVARIATE CASE ###########
+
+
+
+
+# simple moving average 
+sma(day_train_forecast$cnt) -> test
+
+#Fit an AR(2) model to each rolling origin subset
+far2 <- function(x, h){sma(day_train_forecast$cnt, order = h)}
+e <- tsCV(day_train_forecast$cnt, far2, h=13)
+
+tsCV(day_train_forecast$cnt, forecastfunction = sma)
+
+# set as time series object with 
+# weekly, monthly and yearly seasonality
+y <- msts(day_train_forecast$cnt, seasonal.periods=c(7,30.5, 365.25))
+
+
+# CUBIC SMOOTHING SPLINES: https://www.rdocumentation.org/packages/forecast/versions/8.9/topics/splinef
+# same as arima (0, 2, 2)
+# shown how cubic smoothing splines can be used to obtain local linear forecasts for a univariate time series
+cubic_smoothing_spline <- splinef(y)
+fcast_cubic_spline <- forecast(cubic_smoothing_spline,  h = nrow(day_test_forecast))
+plot(fcast_cubic_spline)
+
+far2 <- function(x, h){forecast(splinef(y,  h = nrow(day_test_forecast)), h=h)}
+e <- tsCV(y, far2, h=1)
+
+# NEURAL NETWORK - DO IN PYTHON!!!
+# fit <- nnetar(y)
+# plot(forecast(fit,h=nrow(day_test_forecast)))
+# lines(y)
+
+
+# Fit MLP
+mlp.fit <- mlp(y)
+frc <- forecast(mlp.fit,h=nrow(day_test_forecast))
+plot(frc)
+
+fit6 <- elm(y)
+frc <- forecast(fit6,h=nrow(day_test_forecast))
+plot(frc)
+
+# ARIMA MODELLING (WITHOUT COVARIATES)
+#y <- msts(day_train_forecast$cnt, seasonal.periods=c(7, 365.25))
+y <- ts(day_train_forecast$cnt, frequency= 7)
+
+fit <- auto.arima(day_train_forecast$cnt)
+autoplot(fit)
+
+day_train_forecast$prewhite <- fit$residuals
+
+fcast <- forecast(fit, h = nrow(day_test_forecast))
+plot(fcast)
+
+# LINEAR MODEL WITH TREND & SEASONAL DUMMIES
+
+
+# EXPONENTIAL SMOOTHING
+
+
+# KERNEL RIDGE REGRESSION
+
+
+# set as time series  https://robjhyndman.com/hyndsight/dailydata/
+y <- ts(day_train_forecast$cnt, frequency= 7)
+
+
+day[, c("temp", "hum",
+        "windspeed", "holiday", "workingday", "weathersit", "temp")] -> m
+# set as time-series: 
+#modelcv <- CVar(y, k=5, lambda=0.15)
+x <- day_train_forecast$cnt
+#y <- ts(day_train_forecast$cnt, frequency=c(7,365.25))
+y <- msts(day_train_forecast$cnt, seasonal.periods=c(7,31, 365.25))
+#y <- msts(x, seasonal.periods=c(7,365.25))
+#y <- ts(x, frequency=7)
+#fit <- tbats(y)
+#fc <- forecast(fit)
+#plot(fc)
+
+# fit <- tbats(y)
+# fc <- forecast(fit)
+# plot(fc)
+
+covs <- as.matrix(day_train_forecast[, c("temp", "hum", "windspeed", "holiday", 
+                          "workingday", "weathersit")])
+covs_pred <- as.matrix(day_test_forecast[, c("temp", "hum", "windspeed", "holiday", 
+                                         "workingday", "weathersit")])
+fit <- auto.arima(y, xreg = covs)
+autoplot(fit)
+fcast <- forecast(fit, xreg = covs_pred, h = nrow(day_test_forecast))
+plot(fcast)
+# calcualte mse
+mse <- mean((day_test_forecast$cnt - fcast$mean)^2)
+mse
+day_test_forecast$predicted <- fcast$mean
+
+# neural network autoregression 
+modelcv <- CVar(lynx, k=5, lambda=0.15)
+print(modelcv)
+
+
+TSA::prewhiten(x, y, x.model = fit)
+
+day$combined <- NA 
+day[1:length(day_train_forecast), ]$combined <- fit$fitted
+rbind(fit$fitted,day_test_forecast$predicted )
+
+day_test_forecast$predicted <- fcast$mean
+plot(day_train_forecast$dteday, day_train_forecast$cnt, col = 3, type = "l")
+lines(day_train_forecast$dteday, fit$fitted, col = 4)
+
+plot(day_test_forecast$dteday, day_test_forecast$cnt, col = 3, type = "l")
+lines(day_test_forecast$dteday, day_test_forecast$predicted, col = 4)
+
+far2 <- function(x, h){forecast(Arima(y, order=c(1,1,1)), h=h)}
+e <- tsCV(y, far2, h=1)
+mean(e^2, na.rm = TRUE)
+
+aic_vals_temp <- NULL
+aic_vas <- NULL 
+
+for(i in 1:5) {
+  for(j in 1:5) {
+    xreg1 <- fourier()
+  }
+}
+
+
+y <- ts(day$cnt, frequency=7)
+z <- fourier(ts(day$cnt, frequency=365.25), K=5)
+zf <- fourier(ts(day$cnt, frequency=365.25), K=5, h=100)
+fit <- auto.arima(y, xreg=cbind(day$workingday,day$yr), seasonal=FALSE)
+fc <- forecast(fit, xreg=cbind(day$workingday,day$yr), h=100)
+plot(fc)
+
+fit <- auto.arima(y)
+autoplot(fit)
+fcast <- forecast(fit)
+plot(fcast)
+
+ts <- ts(day$cnt, start=c(2011,1,1),frequency=365.25)
+plot.ts(ts)
+plot(decompose(ts))
+plot(stl(ts,
+         s.window="periodic"))
+MK = MannKendall(ts)
+SMK = SeasonalMannKendall(ts)
+sea.sens.slope(ts)
+
+x <- day$cnt
+ets(x)
+fit <- tbats(x)
+seasonal <- !is.null(fit$seasonal)
+seasonal
+
+# create numeric day/hour counter (starting counting with 1 at the beginning of the period)
+hour[ , day_counter := .GRP, by = c("dteday")]
+hour[ , time_counter := day_counter + hr - 1 + (day_counter - 1)*23]
+day[, time_counter := .GRP, by = c("dteday")]
+
+# obtain results for prediciton on equally spacex N = 200 Xs
+results_test <- kernel_predictions(df = hour_train_rand, 
+                                   test_df = hour_test_rand, 
+                                   myx = "hr", 
+                                   myy = "cnt",
+                                   a = min(hour$hr),
+                                   b = max(hour$hr),
+                                   m = 10) 
+
+# overall (day-hour)
+regressogram(hour$time_counter, hour$cnt)
+
+# ARIMA(1,0,2)(0,1,0)[365] with drift 
+fit <- auto.arima(y)
+autoplot(fit)
+fcast <- forecast(fit)
+plot(fcast)
+plot(forecast(fit))
+# day only 
+regressogram(day$time_counter, day$cnt)
+
+if (class(fit2) != "Arima" && class(fit2) != "estimate") {
+  print("STOP")
+}
+
+fit2 <- fit
+class(fit2) <- c("Arima", "forecast_ARIMA", "ARIMA")
+
+
+
+
